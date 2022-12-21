5025bec3067447d89b0a8f6e586c4ec8f9971193
KristianBic <92215832+KristianBic@users.noreply.github.com>
2022-12-21 21:57:49 +0100

    Init

---
+++ cmake-build-debug/CMakeFiles/SemestralnaPracaPOS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SemestralnaPracaPOS.dir/main.c.o"
+  "SemestralnaPracaPOS"
+  "SemestralnaPracaPOS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SemestralnaPracaPOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
