7583c4bf8b3d9f19fed80ae6d1c9a314de3dbf24
KristianBic <92215832+KristianBic@users.noreply.github.com>
2022-12-29 17:29:19 +0100

    UPDATE: New connection, HTTP download

--- Client.c
+++ Client.c
@@ -9,10 +9,13 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <fcntl.h>
+
 
 /* The DEFAULT IP address and port number to connect to */
-#define DEFAULT_IPADDR "212.183.159.230"
-//#define DEFAULT_IPADDR "frios2.fri.uniza.sk"
+#define DEFAULT_IPADDR "example.com"
 #define DEFAULT_PORTNUM 80
 
 #ifndef INADDR_NONE
@@ -21,7 +24,7 @@
 
 char *address; //will be displayed after every command
 int port;
-curl_socket_t sockfd;
+int sockfd;
 
 struct FtpFile {
     const char *filename;
@@ -47,27 +50,27 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
     return written;
 }
 
-static int closecb(void *clientp, curl_socket_t item)
+static int closecb(void *clientp, int item)
 {
     (void)clientp;
     printf("libcurl wants to close %d now\n", (int)item);
     return 0;
 }
 
-static curl_socket_t opensocket(void *clientp,
+static int opensocket(void *clientp,
                                 curlsocktype purpose,
                                 struct curl_sockaddr *address)
 {
-    curl_socket_t sockfd;
+    int sockfd;
     (void)purpose;
     (void)address;
-    sockfd = *(curl_socket_t *)clientp;
+    sockfd = *(int *)clientp;
     /* the actual externally set socket is passed in via the OPENSOCKETDATA
        option */
     return sockfd;
 }
 
-static int sockopt_callback(void *clientp, curl_socket_t curlfd,
+static int sockopt_callback(void *clientp, int curlfd,
                             curlsocktype purpose)
 {
     (void)clientp;
@@ -77,17 +80,18 @@ static int sockopt_callback(void *clientp, curl_socket_t curlfd,
     return CURL_SOCKOPT_ALREADY_CONNECTED;
 }
 
-void download(curl_socket_t sockfd) {
+void download(int sockfd) {
     CURL *curl;
     CURLcode res;
     struct FtpFile ftpfile = {
-            "prvy.c", /* name to store the file as if successful */
+            "prvy.txt", /* name to store the file as if successful */
             NULL
     };
 
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://99.99.99.99:9999");
+        //curl_easy_setopt(curl, CURLOPT_URL, "http://99.99.99.99:9999");
+        curl_easy_setopt(curl, CURLOPT_URL, "http://212.183.159.230/5MB.zip");
         //curl_easy_setopt(curl, CURLOPT_USERPWD, "meno:heslo");
 
         /* no progress meter please */
@@ -129,40 +133,105 @@ void download(curl_socket_t sockfd) {
     }
     curl_global_cleanup();
 }
+void downloadHTMLfromHTTP(int sockfd) {
+    char buf[2056];
+    int byte_count;
 
-int serverConnection() {
-    curl_socket_t sockfd;
-    struct sockaddr_in server;
+    char *header = "GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n";
+    send(sockfd,header,strlen(header),0);
+    printf("GET Sent...\n");
+    //all right ! now that we're connected, we can receive some data!
+    byte_count = recv(sockfd,buf,sizeof(buf),0);
+    printf("recv()'d %d bytes of data in buf\n",byte_count);
+    printf("%.*s",byte_count,buf); // <-- give printf() the actual data size
+}
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); //vytvorenie soketu
-    if(sockfd == CURL_SOCKET_BAD) { //sockfd == -1
-        perror("Error pri vytvarani socketu\n");
-        close(sockfd);
-        exit(3);
+void downloadHTTP(int sockfd) {
+    char *fileName = "stvrty.txt";
+    struct FtpFile ftpfile = { "prvy.txt", NULL};
+    int fd;
+
+    if(access(fileName, F_OK) == 0) { //if file exists
+        printf("Subor s rovnakym nazvom uz existuje. Chcete ho prepisat? (a/n): ");
+        char answer[256];
+        fgets(answer, sizeof(answer), stdin);
+        answer[0] = tolower(answer[0]);
+
+        if(answer[0] == 'a') {
+            printf("Prepisujem subor. Pokracujeme v stahovani.\n");
+        } else if(answer[0] == 'n') {
+            printf("Canceling download\n");
+            return;
+        } else {
+            printf("Error: Zly vstup. Rusi sa stahovanie.\n");
+            return;
+        }
+    } else {
+        printf("Pokracujeme v stahovani.\n");
     }
 
-    memset(&server, 0, sizeof(server));
+    if((fd = open(fileName, O_WRONLY | O_CREAT, 0666)) == -1) { //open file
+        printf("Error. Subor sa neda otvorit\n");
+        return;
+    }
+    printf("Stahovanie suboru %s...\n", fileName);
+
+    char recv_data[2056];
+    char* path = "index.html";
+    char* domain = "www.example.com";
+    char send_data[2056];
+    snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\\r\\nHost: %s\\r\\n\\r\\n\"", path, domain);
+
+    send(sockfd,send_data,strlen(send_data),0);
+    printf("Send data...\n");
+    recv(sockfd,recv_data,sizeof(recv_data),0);
+
+    if(write(fd, recv_data, strlen(recv_data)) == -1) {
+        perror("Error writing to file");
+        close(fd);
+        return;
+    }
+    printf("Stahovanie uspesne\n");
+
+    close(fd);
+}
+
+int serverConnection() {
+    struct sockaddr_in server;
+    struct hostent *he;
+    struct in_addr **addr_list;
+    char *ipaddr;
+    int sockfd;
+
+    if((he = gethostbyname(address)) == NULL) {
+        herror("Error resolving hostname");
+        exit(-1);
+    } //end if
+
+    addr_list = (struct in_addr **) he->h_addr_list;
 
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_addr(address); // musi tam byt zrejme IP-cka
+    server.sin_addr = *addr_list[0];
 
-    if(INADDR_NONE == server.sin_addr.s_addr) {
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { //create socket
+        perror("Socket failed");
         close(sockfd);
-        exit(2);
-    }
+        exit(-1);
+    } //end if
 
-    printf("Pripajanie ku %s...\n\n", address);
-    if(connect(sockfd, (struct sockaddr *) &server, sizeof(server)) == -1) {
+    ipaddr = inet_ntoa(*addr_list[0]);
+    printf("Connecting to %s [%s]...\n\n", address, ipaddr);
+    if(connect(sockfd, (struct sockaddr *)&server,
+               sizeof(struct sockaddr_in)) == -1) { //create connection
+        perror("Connection failed");
         close(sockfd);
-        printf("client error: connect: %s\n", strerror(errno));
-        exit(1);
-    }
-    printf("Pripojenie prebehlo uspesne...\n\n\n");
+        exit(-1);
+    } //end if
 
+    printf("Successful Connection. \n");
     return sockfd;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -180,10 +249,16 @@ int main(int argc, char *argv[])
         address = argv[1];
         port = atoi(argv[2]);
     }
+    if (port > 65536 || port < 0) {
+        printf("Invalid Port Number!");
+        exit(5);
+    }
     printf("\n");
 
     sockfd = serverConnection();
-    download(sockfd);
+    downloadHTTP(sockfd);
+    //downloadHTMLfromHTTP(sockfd);
+    //download(sockfd);
 
     return 0;
 }
